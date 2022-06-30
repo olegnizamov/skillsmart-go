9b9f6909de8f8219541edc622765b08fcbf4c3df
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2022-06-30 17:40:48 +0400

    LinkedList

---
+++ aisd01_task1/go.go
@@ -0,0 +1,205 @@
+package aisd01_task1
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	next  *Node
+	value int
+}
+
+type LinkedList struct {
+	head *Node
+	tail *Node
+}
+
+func (l *LinkedList) AddInTail(item Node) {
+	if l.head == nil {
+		l.head = &item
+	} else {
+		l.tail.next = &item
+	}
+
+	l.tail = &item
+}
+
+func (l *LinkedList) Count() int {
+	var node = l.head
+	var count int = 0
+	for {
+		if node == nil {
+			break
+		}
+		count++
+		node = node.next
+	}
+
+	return count
+}
+
+func (l *LinkedList) Find(n int) Node {
+
+	var node = l.head
+
+	for {
+
+		if node == nil {
+			break
+		}
+
+		if node.value == n {
+			return Node{value: node.value, next: node.next}
+		}
+
+		node = node.next
+	}
+
+	return Node{value: -1, next: nil}
+}
+
+func (l *LinkedList) FindAll(n int) []Node {
+	var nodes []Node
+	var node = l.head
+	for {
+
+		if node == nil {
+			break
+		}
+
+		if node.value == n {
+			nodes = append(nodes, Node{value: node.value, next: node.next})
+		}
+
+		node = node.next
+	}
+
+	return nodes
+}
+
+func (l *LinkedList) Delete(n int, all bool) {
+
+	var foundElement bool = false
+
+	if l.head == nil {
+		return
+	}
+
+	if l.head.value == n {
+		l.head = l.head.next
+		if l.head == nil {
+			l.tail = nil
+		}
+		foundElement = true
+	} else {
+		var currentNode = l.head.next
+		var prevNode = l.head
+
+		for {
+			if currentNode == nil {
+				break
+			}
+
+			if currentNode.value == n {
+				if l.tail == currentNode {
+					l.tail = prevNode
+					l.tail.next = nil
+					foundElement = true
+					break
+				}
+
+				prevNode.next = currentNode.next
+				foundElement = true
+				break
+			}
+
+			prevNode = currentNode
+			currentNode = currentNode.next
+		}
+	}
+
+	if all == true && foundElement == true {
+		l.Delete(n, all)
+	}
+}
+
+func (l *LinkedList) Insert(after *Node, add Node) {
+
+	if after == nil {
+		l.InsertFirst(add)
+		return
+	}
+
+	var node = l.head
+	for {
+
+		if node == nil {
+			break
+		}
+
+		if node.value == after.value {
+			if node == l.tail {
+				l.tail = &add
+			}
+			add.next = node.next
+			node.next = &add
+			return
+		}
+		node = node.next
+	}
+
+}
+
+func (l *LinkedList) InsertFirst(first Node) {
+
+	if l.head == nil {
+		l.head = &first
+		l.tail = &first
+		return
+	}
+
+	var oldHead *Node = l.head
+	l.head = &first
+	l.head.next = oldHead
+}
+
+func (l *LinkedList) Clean() {
+	l.head = nil
+	l.tail = nil
+}
+
+func (l *LinkedList) print() {
+	var node = l.head
+
+	for {
+
+		if node == nil {
+			break
+		}
+		fmt.Println(node.value)
+		node = node.next
+	}
+
+}
+
+func linkedListSum(list1 *LinkedList, list2 *LinkedList) *LinkedList {
+	var result = new(LinkedList)
+	if list1.Count() != list2.Count() {
+		return &LinkedList{head: nil, tail: nil}
+	}
+	var node1 *Node = list1.head
+	var node2 *Node = list2.head
+
+	for {
+
+		if node1 == nil {
+			break
+		}
+		result.AddInTail(Node{value: node1.value + node2.value})
+		node1 = node1.next
+		node2 = node2.next
+	}
+
+	return result
+
+}
---
+++ aisd01_task1/go_test.go
@@ -0,0 +1,398 @@
+package aisd01_task1
+
+import "testing"
+
+func TestCountHasElements(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+
+	var v int
+	v = linkedlist.Count()
+	if v != 5 {
+		t.Error("Expected 5, got ", v)
+	}
+}
+
+func TestCountZero(t *testing.T) {
+
+	var linkedlist = new(LinkedList)
+	var v int
+	v = linkedlist.Count()
+	if v != 0 {
+		t.Error("Expected 0, got ", v)
+	}
+}
+
+func TestCleanHasElements(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+
+	linkedlist.Clean()
+	if nil != linkedlist.head || nil != linkedlist.tail {
+		t.Error("Expected nil LinkedList , got not nil")
+	}
+}
+
+func TestCleanZero(t *testing.T) {
+
+	var linkedlist = new(LinkedList)
+	linkedlist.Clean()
+	if nil != linkedlist.head || nil != linkedlist.tail {
+		t.Error("Expected nil LinkedList , got not nil")
+	}
+}
+
+func TestFindZero(t *testing.T) {
+
+	var linkedlist = new(LinkedList)
+	var result Node = linkedlist.Find(3)
+	var answer Node = Node{value: -1, next: nil}
+	if answer != result {
+		t.Error("Expected Node{value:-1, next: nil}, got ", result)
+	}
+}
+
+func TestFindHasElements(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+
+	var result Node = linkedlist.Find(3)
+	var answer Node = Node{value: 3, next: &node4}
+	if answer.value != result.value && answer.next.value != result.next.value {
+		t.Error("Expected Node{value:3, next: &node4}, got ", result)
+	}
+}
+
+func TestFindHasElements2(t *testing.T) {
+
+	var node1 Node = Node{value: 0}
+	var node2 Node = Node{value: 0}
+	var node3 Node = Node{value: 1}
+	var node4 Node = Node{value: 1}
+	var node5 Node = Node{value: 1}
+	var node6 Node = Node{value: 2}
+	var node7 Node = Node{value: 2}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node6)
+	linkedlist.AddInTail(node7)
+
+	var result Node = linkedlist.Find(1)
+	var answer Node = Node{value: 1, next: &node4}
+	if answer.value != result.value && answer.next.value != result.next.value {
+		t.Error("Expected Node{value:3, next: &node4}, got ", result)
+	}
+}
+
+func TestSumSameLength(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+
+	var linkedlist2 = new(LinkedList)
+	linkedlist2.AddInTail(node1)
+	linkedlist2.AddInTail(node2)
+	linkedlist2.AddInTail(node3)
+	linkedlist2.AddInTail(node4)
+	linkedlist2.AddInTail(node5)
+
+	var result = linkedListSum(linkedlist, linkedlist2)
+	var answer = new(LinkedList)
+
+	var node1Answer Node = Node{value: 2}
+	var node2Answer Node = Node{value: 4}
+	var node3Answer Node = Node{value: 6}
+	var node4Answer Node = Node{value: 8}
+	var node5Answer Node = Node{value: 10}
+
+	answer.AddInTail(node1Answer)
+	answer.AddInTail(node2Answer)
+	answer.AddInTail(node3Answer)
+	answer.AddInTail(node4Answer)
+	answer.AddInTail(node5Answer)
+
+	var nodeResult *Node = result.head
+	var nodeAnswer *Node = answer.head
+
+	for {
+
+		if nodeResult == nil {
+			break
+		}
+
+		if nodeResult.value != nodeAnswer.value {
+			t.Error("Expected , got ", nodeResult.value)
+		}
+
+		nodeResult = nodeResult.next
+		nodeAnswer = nodeAnswer.next
+	}
+
+}
+
+func TestSumNotSameLength(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+
+	var linkedlist2 = new(LinkedList)
+	linkedlist2.AddInTail(node1)
+	linkedlist2.AddInTail(node2)
+	linkedlist2.AddInTail(node3)
+	linkedlist2.AddInTail(node4)
+	linkedlist2.AddInTail(node5)
+	linkedlist2.AddInTail(Node{value: 6})
+	linkedlist2.AddInTail(Node{value: 7})
+
+	var result = linkedListSum(linkedlist, linkedlist2)
+
+	if result.head != nil && result.tail != nil {
+		t.Error("Expected nil, got ", result)
+	}
+}
+
+func TestFindAllHasNot(t *testing.T) {
+
+	var linkedlist = new(LinkedList)
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	var resultNodes []Node = linkedlist.FindAll(100)
+
+	if len(resultNodes) > 0 {
+		t.Error("Expected zero count, got ", len(resultNodes))
+	}
+
+}
+
+func TestFindAllHasManyElements(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	var resultNodes []Node = linkedlist.FindAll(5)
+
+	if len(resultNodes) != 3 {
+		t.Error("Expected 3 count, got ", len(resultNodes))
+	}
+}
+
+func TestFindAllHasOneElement(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	var resultNodes []Node = linkedlist.FindAll(2)
+
+	if len(resultNodes) != 1 {
+		t.Error("Expected 1 count, got ", len(resultNodes))
+	}
+}
+
+func TestInsertFirstNilList(t *testing.T) {
+
+	var node1 Node = Node{value: 1}
+	var linkedlist = new(LinkedList)
+	linkedlist.InsertFirst(node1)
+
+	if linkedlist.head.value != 1 && linkedlist.tail.value != 1 {
+		t.Error("Expected 1, got ", linkedlist.head.value)
+	}
+}
+
+func TestInsertFirsHasHead(t *testing.T) {
+
+	var nodeNewHead Node = Node{value: 100}
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	linkedlist.InsertFirst(nodeNewHead)
+
+	if linkedlist.head.value != 100 {
+		t.Error("Expected 100, got ", linkedlist.head.value)
+	}
+}
+
+func TestInsert(t *testing.T) {
+
+	var nodeNewHead Node = Node{value: 100}
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	var linkedlist = new(LinkedList)
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	linkedlist.AddInTail(node5)
+	linkedlist.Insert(&node2, nodeNewHead)
+
+	if linkedlist.head.next.next.value != 100 {
+		t.Error("Expected 100, got ", linkedlist.head.value)
+	}
+}
+
+func TestDeleteHeadNil(t *testing.T) {
+	var linkedlist = new(LinkedList)
+	linkedlist.Delete(1, false)
+
+	if linkedlist.head != nil {
+		t.Error("Expected nil, got ", linkedlist.head.value)
+	}
+}
+
+func TestDeleteHead(t *testing.T) {
+	var linkedlist = new(LinkedList)
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.Delete(1, false)
+
+	if linkedlist.head.value != 2 {
+		t.Error("Expected 2, got ", linkedlist.head.value)
+	}
+	linkedlist.Delete(2, false)
+
+	if linkedlist.head != nil && linkedlist.tail != nil {
+		t.Error("Expected nil, got ", linkedlist.head)
+	}
+}
+
+func TestDeleteOneTime(t *testing.T) {
+	var linkedlist = new(LinkedList)
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 3}
+	var node4 Node = Node{value: 4}
+	var node5 Node = Node{value: 5}
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+	linkedlist.Delete(3, false)
+
+	if linkedlist.head.next.next.value == 3 {
+		t.Error("Expected 4, got ", linkedlist.head.value)
+	}
+}
+
+func TestDeleteManyTime(t *testing.T) {
+	var linkedlist = new(LinkedList)
+	var node1 Node = Node{value: 1}
+	var node2 Node = Node{value: 2}
+	var node3 Node = Node{value: 1}
+	var node4 Node = Node{value: 1}
+	var node5 Node = Node{value: 1}
+	linkedlist.AddInTail(node1)
+	linkedlist.AddInTail(node2)
+	linkedlist.AddInTail(node3)
+	linkedlist.AddInTail(node4)
+	linkedlist.AddInTail(node5)
+
+	linkedlist.Delete(1, true)
+
+	if linkedlist.head.value != 2 {
+		t.Error("Expected 2, got ", linkedlist.head.value)
+	}
+
+	if linkedlist.Count() != 1 {
+		t.Error("Expected 1 count, got ", linkedlist.Count())
+	}
+}
