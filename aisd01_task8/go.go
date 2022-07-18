f1c9bc37b3c98b0bf96aba8ed5cfb39d7c6757c7
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2022-07-18 14:56:43 +0400

    Хэширование

---
+++ aisd01_task8/go.go
@@ -0,0 +1,86 @@
+package main
+
+type HashTable struct {
+	size  int
+	step  int
+	slots []string
+}
+
+func Init(sz int, stp int) HashTable {
+	ht := HashTable{size: sz, step: stp, slots: nil}
+	ht.slots = make([]string, sz)
+	return ht
+}
+
+func (ht *HashTable) HashFun(value string) int {
+	// всегда возвращает корректный индекс слота
+	var index = len(value) % ht.size
+	return index
+}
+
+func (ht *HashTable) SeekSlot(value string) int {
+	// находит индекс пустого слота для значения,
+	// или -1
+	var index = ht.HashFun(value)
+	if ht.slots[index] == "" {
+		return index
+	}
+	var i = 0
+	for {
+		if i >= ht.size {
+			break
+		}
+		index += ht.step
+
+		if index >= ht.size {
+			index = index - ht.size
+		}
+		if ht.slots[index] == "" {
+			return index
+		} else {
+			i += 1
+		}
+	}
+
+	return -1
+}
+
+func (ht *HashTable) Put(value string) int {
+	// записываем значение по хэш-функции
+	var slot = ht.SeekSlot(value)
+	if slot != -1 {
+		ht.slots[slot] = value
+	}
+	return slot
+	// возвращается индекс слота или -1
+	// если из-за коллизий элемент не удаётся разместить
+}
+
+func (ht *HashTable) Find(value string) int {
+	// находит индекс слота со значением, или -1
+	var hashIndex = ht.HashFun(value)
+
+	if ht.slots[hashIndex] == value {
+		return hashIndex
+	}
+
+	var i = 0
+	for {
+		if i >= ht.size {
+			break
+		}
+		hashIndex += ht.step
+
+		if hashIndex >= ht.size {
+			hashIndex = hashIndex - ht.size
+		}
+		if ht.slots[hashIndex] == value {
+			return hashIndex
+		} else {
+			i += 1
+		}
+	}
+
+	// находит индекс слота со значением, или -1
+	return -1
+}
---
+++ aisd01_task8/go_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHashFun(t *testing.T) {
+	var ht = Init(17, 3)
+	var result = ht.HashFun("aaa")
+	if result != 3 {
+		t.Error("Error")
+	}
+
+	ht = Init(17, 3)
+	result = ht.HashFun("aa")
+	if result != 2 {
+		t.Error("Error")
+	}
+
+	ht = Init(17, 3)
+	result = ht.HashFun("aab")
+	if result != 3 {
+		t.Error("Error")
+	}
+
+	ht = Init(17, 3)
+	result = ht.HashFun("a")
+	if result != 1 {
+		t.Error("Error")
+	}
+}
+
+func TestPut(t *testing.T) {
+	var ht = Init(17, 3)
+	var result1 = ht.Put("aaa")
+	var result2 = ht.Put("aa")
+
+	if result1 != 3 {
+		t.Error("Error")
+	}
+
+	if result2 != 2 {
+		t.Error("Error")
+	}
+}
+
+func TestSeekSlot(t *testing.T) {
+
+	var ht = Init(17, 3)
+	var result = ht.SeekSlot("aaa")
+	if result != 3 {
+		t.Error("Error")
+	}
+
+	ht = Init(17, 3)
+	result = ht.HashFun("aa")
+	if result != 2 {
+		t.Error("Error")
+	}
+
+}
+
+func TestSeekSlotAndPut(t *testing.T) {
+
+	var ht = Init(17, 3)
+	_ = ht.Put("aaa")
+	_ = ht.Put("aa")
+	_ = ht.Put("bb")
+	_ = ht.Put("bbb")
+	_ = ht.Put("cc")
+	_ = ht.Put("ccc")
+	_ = ht.Put("dd")
+	_ = ht.Put("ddd")
+	_ = ht.Put("ee")
+	_ = ht.Put("eee")
+	_ = ht.Put("ff")
+	_ = ht.Put("fff")
+	_ = ht.Put("gg")
+	_ = ht.Put("ggg")
+	_ = ht.Put("vv")
+	_ = ht.Put("vvv")
+
+	var result1 = ht.Put("ww")
+	var result2 = ht.Put("www")
+
+	if result1 != 16 {
+		t.Error("Error")
+	}
+
+	if result2 != -1 {
+		t.Error("Error")
+	}
+
+}
+
+func TestFind(t *testing.T) {
+
+	var ht = Init(17, 3)
+	_ = ht.Put("aaa")
+	_ = ht.Put("aa")
+	_ = ht.Put("bb")
+	_ = ht.Put("bbb")
+	_ = ht.Put("cc")
+	_ = ht.Put("ccc")
+	_ = ht.Put("dd")
+	_ = ht.Put("ddd")
+	_ = ht.Put("ee")
+	_ = ht.Put("eee")
+	_ = ht.Put("ff")
+	_ = ht.Put("fff")
+	_ = ht.Put("gg")
+	_ = ht.Put("ggg")
+	_ = ht.Put("vv")
+	_ = ht.Put("vvv")
+
+	var result1 = ht.Find("vv")
+	var result2 = ht.Find("www")
+
+	if result1 == -1 {
+		t.Error("Error")
+	}
+
+	if result2 != -1 {
+		t.Error("Error")
+	}
+
+}
