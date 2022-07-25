d12de3568962147e6f2ba750606a1a8aa18f279d
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2022-07-25 20:03:44 +0400

    Множества

---
+++ aisd01_task10/go.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	//	"os"
+	//	"strconv"
+	"constraints"
+)
+
+type PowerSet[T constraints.Ordered] struct {
+	// ваша реализация хранилища
+	values []T
+}
+
+func (ps *PowerSet[T]) Size() int {
+	// количество элементов в множестве
+	return len(ps.values)
+}
+
+func (ps *PowerSet[T]) Put(value T) {
+	if !ps.Get(value) {
+		ps.values = append(ps.values, value)
+	}
+	// всегда срабатывает
+}
+
+func (ps *PowerSet[T]) Compare(v1 T, v2 T) int {
+	if v1 < v2 {
+		return -1
+	}
+	if v1 > v2 {
+		return +1
+	}
+	return 0
+}
+
+func (ps *PowerSet[T]) Get(value T) bool {
+
+	// возвращает true если value имеется в множестве
+	var index = 0
+	for {
+		if index >= ps.Size() || ps.Size() < 1 {
+			break
+		}
+
+		if ps.Compare(ps.values[index], value) == 0 {
+			return true
+		}
+
+		index += 1
+	}
+
+	return false
+}
+
+func (ps *PowerSet[T]) Remove(value T) bool {
+	// возвращает true если value удалено
+	// возвращает true если value имеется в множестве
+	var defaultTypeElement T
+
+	var index = 0
+	for {
+
+		if index >= ps.Size() || ps.Size() < 1 {
+			break
+		}
+
+		if ps.Compare(ps.values[index], value) == 0 {
+			ps.values[index] = defaultTypeElement
+			return true
+		}
+
+		index += 1
+	}
+
+	return false
+}
+
+func (ps *PowerSet[T]) Intersection(set2 PowerSet[T]) PowerSet[T] {
+	// пересечение текущего множества и set2
+	var result PowerSet[T]
+	if ps.Size() >= set2.Size() {
+		var index = 0
+		for {
+			if index >= ps.Size() || ps.Size() < 1 {
+				break
+			}
+
+			if set2.Get(ps.values[index]) {
+				result.Put(ps.values[index])
+			}
+			index += 1
+		}
+	} else {
+		var index = 0
+		for {
+			if index >= set2.Size() || set2.Size() < 1 {
+				break
+			}
+
+			if ps.Get(set2.values[index]) {
+				result.Put(set2.values[index])
+			}
+
+			index += 1
+
+		}
+	}
+
+	return result
+}
+
+func (ps *PowerSet[T]) Union(set2 PowerSet[T]) PowerSet[T] {
+	// объединение текущего множества и set2
+	var result PowerSet[T]
+	var index = 0
+	for {
+		if index >= ps.Size() || ps.Size() < 1 {
+			break
+		}
+		result.Put(ps.values[index])
+
+		index += 1
+
+	}
+
+	index = 0
+	for {
+		if index >= set2.Size() || set2.Size() < 1 {
+			break
+		}
+
+		result.Put(set2.values[index])
+
+		index += 1
+	}
+
+	return result
+}
+
+func (ps *PowerSet[T]) Difference(set2 PowerSet[T]) PowerSet[T] {
+	// разница текущего множества и set2
+	var result PowerSet[T]
+
+	var index = 0
+	for {
+
+		if index >= ps.Size() || ps.Size() < 1 {
+			break
+		}
+
+		if !set2.Get(ps.values[index]) {
+			result.Put(ps.values[index])
+		}
+
+		index += 1
+
+	}
+	return result
+}
+
+func (ps *PowerSet[T]) IsSubset(set2 PowerSet[T]) bool {
+	// возвращает true, если set2 есть
+	// подмножество текущего множества
+
+	var index = 0
+	for {
+
+		if index >= set2.Size() || set2.Size() < 1 {
+			break
+		}
+
+		if !ps.Get(set2.values[index]) {
+			return false
+		}
+
+		index += 1
+
+	}
+
+	return true
+}
---
+++ aisd01_task10/go_test.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPowerSet_Put(t *testing.T) {
+	var result = PowerSet[string]{}
+
+	result.Put("1")
+	if len(result.values) != 1 {
+		t.Error("Error")
+	}
+
+	result.Put("2")
+	if len(result.values) != 2 {
+		t.Error("Error")
+	}
+
+	result.Put("3")
+	if len(result.values) != 3 {
+		t.Error("Error")
+	}
+	result.Put("3")
+	if len(result.values) != 3 {
+		t.Error("Error")
+	}
+	result.Put("4")
+	if len(result.values) != 4 {
+		t.Error("Error")
+	}
+
+	/*var resultString = PowerSet[string]{}
+	for i := 0; i < 10000; i++ {
+		var strConvert = fmt.Sprint(i)
+		resultString.Put(strConvert)
+	}
+	if len(resultString.values) != 10000 {
+		t.Error("Error")
+	}*/
+
+}
+
+func TestPowerSet_Size(t *testing.T) {
+	var result = PowerSet[string]{}
+
+	result.Put("1")
+	if result.Size() != 1 {
+		t.Error("Error")
+	}
+
+	result.Put("2")
+	if result.Size() != 2 {
+		t.Error("Error")
+	}
+
+	result.Put("3")
+	if result.Size() != 3 {
+		t.Error("Error")
+	}
+	result.Put("3")
+	if result.Size() != 3 {
+		t.Error("Error")
+	}
+	result.Put("4")
+	if result.Size() != 4 {
+		t.Error("Error")
+	}
+}
+
+func TestPowerSet_Get(t *testing.T) {
+	var result = PowerSet[string]{}
+
+	result.Put("1")
+	result.Put("2")
+	result.Put("3")
+
+	var answer = result.Get("2")
+	if answer != true {
+		t.Error("Error")
+	}
+
+	answer = result.Get("1")
+	if answer != true {
+		t.Error("Error")
+	}
+
+	answer = result.Get("100")
+	if answer != false {
+		t.Error("Error")
+	}
+}
+
+func TestPowerSet_Remove(t *testing.T) {
+	var result = PowerSet[string]{}
+
+	result.Put("1")
+	result.Put("2")
+	result.Put("3")
+
+	var answer = result.Remove("2")
+	if answer != true {
+		t.Error("Error")
+	}
+
+	answer = result.Remove("3")
+	if answer != true {
+		t.Error("Error")
+	}
+
+	answer = result.Remove("3")
+	if answer != false {
+		t.Error("Error")
+	}
+	answer = result.Remove("2")
+	if answer != false {
+		t.Error("Error")
+	}
+}
+
+func TestPowerSet_Intersection(t *testing.T) {
+	var set1 = PowerSet[string]{}
+	var set2 = PowerSet[string]{}
+
+	set1.Put("1")
+	set1.Put("2")
+	set1.Put("3")
+	set1.Put("4")
+	set1.Put("5")
+	set1.Put("6")
+
+	set2.Put("2")
+	set2.Put("4")
+	set2.Put("6")
+	set2.Put("8")
+	set2.Put("10")
+
+	var answer = set1.Intersection(set2)
+	if answer.Size() != 3 {
+		t.Error("Error")
+	}
+
+	if answer.values[0] != "2" {
+		t.Error("Error")
+	}
+
+	if answer.values[1] != "4" {
+		t.Error("Error")
+	}
+
+	if answer.values[2] != "6" {
+		t.Error("Error")
+	}
+
+}
+
+func TestPowerSet_Union(t *testing.T) {
+	var set1 = PowerSet[string]{}
+	var set2 = PowerSet[string]{}
+
+	set1.Put("1")
+	set1.Put("2")
+	set1.Put("3")
+	set1.Put("4")
+	set1.Put("5")
+	set1.Put("6")
+
+	set2.Put("2")
+	set2.Put("4")
+	set2.Put("6")
+	set2.Put("8")
+	set2.Put("10")
+
+	var answer = set1.Union(set2)
+	if answer.Size() != 8 {
+		t.Error("Error")
+	}
+
+	if answer.values[0] != "1" {
+		t.Error("Error")
+	}
+
+	if answer.values[7] != "10" {
+		t.Error("Error")
+	}
+
+}
+
+func TestPowerSet_Difference(t *testing.T) {
+	var set1 = PowerSet[string]{}
+	var set2 = PowerSet[string]{}
+
+	set1.Put("1")
+	set1.Put("2")
+	set1.Put("3")
+	set1.Put("4")
+	set1.Put("5")
+	set1.Put("6")
+
+	set2.Put("2")
+	set2.Put("4")
+	set2.Put("6")
+	set2.Put("8")
+	set2.Put("10")
+
+	var answer = set1.Difference(set2)
+	if answer.Size() != 3 {
+		t.Error("Error")
+	}
+
+	if answer.values[0] != "1" {
+		t.Error("Error")
+	}
+
+	if answer.values[1] != "3" {
+		t.Error("Error")
+	}
+
+	if answer.values[2] != "5" {
+		t.Error("Error")
+	}
+}
+
+func TestPowerSet_IsSubset(t *testing.T) {
+	var set1 = PowerSet[string]{}
+	var set2 = PowerSet[string]{}
+
+	set1.Put("1")
+	set1.Put("2")
+	set1.Put("3")
+	set1.Put("4")
+	set1.Put("5")
+	set1.Put("6")
+
+	set2.Put("2")
+	set2.Put("4")
+	set2.Put("6")
+
+	var answer = set1.IsSubset(set2)
+	if answer != true {
+		t.Error("Error")
+	}
+
+	set2.Put("8")
+
+	answer = set1.IsSubset(set2)
+	if answer != false {
+		t.Error("Error")
+	}
+
+}
